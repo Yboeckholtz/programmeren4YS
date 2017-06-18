38544f06299d3797ad5d0eae5be63616923b02f5
Youri van Boeckholtz <Yboeckholtz@gmail.com>
2017-06-18 15:49:26 +0200

    zelfde melding

--- api/authentication.routes.v1.js
+++ api/authentication.routes.v1.js
@@ -14,7 +14,7 @@ var db = require('../config/db');
 // Hier gaat de gebruiker inloggen.
 // Input: username en wachtwoord
 // ToDo: 
-//	 - zoek de username in de database, en vind het password dat opgeslagen is
+//	 - zoek de username in de database, en vind het password wat opgeslagen is 
 // 	 - als user gevonden en password matcht, dan return valide token
 //   - anders is de inlogpoging gefaald - geef foutmelding terug.
 //
